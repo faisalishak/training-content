ae53ca1591eee42a6fceb73c0a3cf027ab9e2d83
faisal <faisal.ishak92@gmail.com>
2020-02-02 19:01:10 +0700

    [update]sisea controller

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -2,10 +2,94 @@
 
 namespace App\Http\Controllers;
 
-use App\Kelas;
 use Illuminate\Http\Request;
 
 class KelasController extends Controller
 {
-   
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kelas = [];
+        return view('adminpanel.kelas.index', compact('kelas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('adminpanel.kelas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return redirect()->route('kelas.index')->with('alert-success', 'Berhasil Menambah Data');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $kelas = (object)[];
+        return view('adminpanel.kelas.edit', compact('kelas'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return redirect()->route('kelas.index')->with('alert-success', 'Berhasil Mengubah Data');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return redirect()->route('kelas.index')->with('alert-success', 'Kelas Berhasil Dihapus');
+    }
 }
--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -9,5 +9,96 @@ use App\Http\Requests\ValidateRegistration;
 
 class SiswaController extends Controller
 {
-    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswa = [];
+
+        return view('adminpanel.siswa.index', compact('siswa'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $arrKelas = [];
+        return view('adminpanel.siswa.create')->with([
+            'arrKelas' => $arrKelas
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return redirect()->route('siswa.index')->with('alert-success', 'Berhasil Menambah Data');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('adminpanel.siswa.edit')->with([
+            'arrKelas' => $arrKelas,
+            'siswa' => $siswa
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return redirect()->route('siswa.index')->with('alert-success', 'Berhasil Mengubah Data');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return redirect()->route('siswa.index')->with('alert-success', 'Siswa Berhasil Dihapus');
+    }
 }
